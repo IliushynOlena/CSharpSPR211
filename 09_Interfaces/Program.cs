048ac35f7c3a649c75ad61ad9095f889747f0555
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-18 12:14:15 +0200

    Interfaces, Standart interfaces

---
+++ 09_Interfaces/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.Text;
+
+namespace _09_Interfaces
+{
+    class IdCard
+    {
+        public void Test()
+        {
+            Console.WriteLine("Hello");
+        }
+        public static void Work()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    public interface IWorker
+    {
+       
+        //public IdCard IdCard { get; set; }
+        public bool IsWorking { get; }
+        string Work();
+        event EventHandler<EventArgs> WorkCompleted;
+    }
+
+
+    abstract class Human//:Object
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public override string ToString()
+        {
+            return $"First Name : {FirstName}\nLast Name : {LastName}\n" +
+                $"Birthday : {Birthday.ToLongDateString()}";
+        }
+    }
+    abstract class Employee: Human
+    {
+        public double Salary { get; set; }
+        public string Position { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nSalary : {Salary}\nPosition : {Position}\n\n";
+        }
+    }
+    interface IWorkable//public
+    {
+        bool IsWorking { get; }
+        string Work();
+    }
+
+    interface IManager
+    {
+        //List<Employee> ListOfWorkers { get; set; }
+        List<IWorkable> ListOfWorkers { get; set; }
+        void Organize();
+        void MakeBudget();
+        void Control();
+    }
+    class Director : Employee, IManager//implement(realize)
+    {
+        public List<IWorkable> ListOfWorkers { get ; set ; }
+
+        public void Control()
+        {
+            Console.WriteLine("Controling work!!!!");
+        }
+
+        public void MakeBudget()
+        {
+            Console.WriteLine("Making budget!!!!");
+        }
+
+        public void Organize()
+        {
+            Console.WriteLine("Organize work!!!");
+        }
+    }
+    class Seller : Employee, IWorkable
+    {
+        bool isWorking = true;
+        public bool IsWorking => isWorking;//get - read only fiel
+        //public bool IsWorking
+        //{ 
+        //    get 
+        //    { 
+        //        return isWorking;
+        //    } 
+        //}
+
+        public string Work()
+        {
+           return "Selling product";
+        }
+    }
+    class Cashier : Employee, IWorkable
+    {
+        bool isWorking = true;
+        public bool IsWorking => isWorking;
+
+        public string Work()
+        {
+            return "Getting pay for product!!!";
+        }
+    }
+    class Administrator : Employee, IManager, IWorkable
+    {
+        public bool IsWorking => true;
+
+        public List<IWorkable> ListOfWorkers { get ; set; }
+
+        public void Control()
+        {
+            Console.WriteLine("Xaxaxaxxaaxxax. I am a BOSS!!!!!");
+        }
+
+        public void MakeBudget()
+        {
+            Console.WriteLine("I have a million!!!!!");
+        }
+
+        public void Organize()
+        {
+            Console.WriteLine("I organize all work in shop!!!");
+        }
+
+        public string Work()
+        {
+            return "I am managing all work!!!((((((((((";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IManager director = new Director
+            {
+                FirstName = "John",
+                LastName = "Grizli",
+                Birthday = new DateTime(1990, 10, 25),
+                Position = "Director",
+                Salary = 300000
+            };
+            Console.WriteLine(director);
+
+            director.MakeBudget();
+
+            IWorkable seller = new Seller
+            {
+                FirstName = "Olga",
+                LastName = "Ivanchuk",
+                Birthday = new DateTime(1995, 5, 17),
+                Position = "Seller",
+                Salary = 7350
+            };
+            Console.WriteLine(seller);
+
+            Console.WriteLine(seller.Work());
+
+            if(seller is Seller)
+                Console.WriteLine($"Seller salary : { (seller as Seller).Salary}");
+
+
+            director.ListOfWorkers = new List<IWorkable>
+            { 
+                 seller,
+                  new Administrator
+                  {
+                      FirstName = "Sasha",
+                    LastName = "Oliunuk",
+                    Birthday = new DateTime(1985, 5, 17),
+                    Position = "Administrator",
+                    Salary = 21000
+                  },
+                  new Cashier
+                  {
+                      FirstName = "Ivanka",
+                    LastName = "Petruc",
+                    Birthday = new DateTime(2001, 5, 17),
+                    Position = "Cashier",
+                    Salary = 1000
+                  }
+            };
+
+            foreach (var worker in director.ListOfWorkers)
+            {
+                Console.WriteLine(worker);
+                if(worker is Administrator)
+                    Console.WriteLine("Administrator");
+
+                if(worker.IsWorking)
+                    Console.WriteLine(worker.Work());
+            }
+
+            Administrator admin = new Administrator();//address
+
+            IManager manager = admin;//address = address
+            manager.Organize();
+
+            IWorkable worker1 = admin;
+            worker1.Work();
+
+        }
+    }
+}
---
+++ 10_StandartInterface/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+
+namespace _10_StandartInterface
+{
+
+    class StudentCard : ICloneable
+    {
+        public int Number { get; set; }
+        public string Series { get; set; }
+
+        public object Clone()
+        {
+            StudentCard copy = this.MemberwiseClone() as StudentCard;
+            return copy;
+        }
+
+        public override string ToString()
+        {
+            return $"Number : {Number}. Series : {Series}";
+        }
+    }
+    class Student : IComparable<Student>, ICloneable
+    {
+        public string FirstName { get; set; }//0x123 - 0x123
+        public string LastName { get; set; }//0x147 - 0x147
+        public DateTime Birthdate { get; set; }//2000-5-9  - 2000-5-9
+        public StudentCard StudentCard { get; set; }//0x336 - 0x336
+
+        public object Clone()
+        {
+            Student copy = (Student)this.MemberwiseClone();
+            //copy.FirstName = (string)this.FirstName.Clone();
+            //copy.LastName = (string)this.LastName.Clone();
+            //copy.StudentCard = new StudentCard()
+            //{
+            //    Number = this.StudentCard.Number,
+            //    Series = this.StudentCard.Series
+            //};
+            copy.StudentCard= this.StudentCard.Clone() as StudentCard;
+
+
+            return copy;
+        }
+
+        public int CompareTo(Student? other)
+        {
+            return FirstName.CompareTo(other.FirstName);
+        }
+     
+
+        public override string ToString()
+        {
+            return $"Name : {FirstName}. Last Name : {LastName}. Birthdate : {Birthdate.ToShortDateString()}" +
+                $"\n{StudentCard}";
+        }
+    }
+    class LastNameComparer : IComparer<Student>
+    {
+        //public int Compare(object? x, object? y)
+        //{
+        //    if (x is Student && y is Student)
+        //    {
+        //        return (x as Student).LastName.CompareTo((y as Student).LastName);
+        //    }
+        //    throw new NotImplementedException();
+        //}
+        public int Compare(Student? x, Student? y)
+        {
+            return x.LastName.CompareTo(y.LastName);
+        }
+    }
+    class BirthdayComparer : IComparer<Student>
+    {
+        public int Compare(Student? x, Student? y)
+        {
+            return x.Birthdate.CompareTo(y.Birthdate);
+        }
+    }
+    class Auditory : IEnumerable
+    {
+        Student[] students = null;//Array
+        public Auditory()
+        {
+            students = new Student[]//Array
+            {
+                new Student
+                {
+                    FirstName = "Bill",
+                    LastName = "Tomson",
+                    Birthdate = new DateTime(2005, 4, 7),
+                    StudentCard = new StudentCard() { Number = 123456, Series = "AA" }
+                },
+            new Student
+            {
+                FirstName = "Olga",
+                LastName = "Ivanchuk",
+                Birthdate = new DateTime(2003, 10, 17),
+                StudentCard = new StudentCard() { Number = 321456, Series = "BA" }
+            },
+            new Student
+            {
+                FirstName = "Candice",
+                LastName = "Leman",
+                Birthdate = new DateTime(2006, 3, 12),
+                StudentCard = new StudentCard() { Number = 7412585, Series = "AA" }
+            },
+            new Student
+            {
+                FirstName = "Nicol",
+                LastName = "Taylor",
+                Birthdate = new DateTime(2004, 7, 14),
+                StudentCard = new StudentCard() { Number = 963258, Series = "BK" }
+            }
+             };
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            
+            return students.GetEnumerator();
+        }
+
+        public void Sort()
+        {
+            Array.Sort(students);
+        }
+        public void Sort(IComparer<Student> comparer)
+        {
+            Array.Sort(students,comparer);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student original = new Student
+            {
+                FirstName = "Bill",
+                LastName = "Tomson",
+                Birthdate = new DateTime(2005, 4, 7),
+                StudentCard = new StudentCard() { Number = 123456, Series = "AA" }
+            };
+            Console.WriteLine(original);
+            Student copy = (original.Clone() as Student)!;
+            copy.StudentCard.Number = 3333333;
+            copy.FirstName = "Lilia";
+            Console.WriteLine(copy);
+            Console.WriteLine(original);
+            Console.WriteLine("__________________________________________");
+
+
+
+
+
+            Auditory auditory = new Auditory();
+            Console.WriteLine("List of Students");
+            foreach (Student st in auditory)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine("-----------------------");
+            auditory.Sort();
+            foreach (Student st in auditory)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine("-----------------------");
+            auditory.Sort(new LastNameComparer());
+            foreach (Student st in auditory)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine("-----------------------");
+            auditory.Sort(new BirthdayComparer());
+            foreach (Student st in auditory)
+            {
+                Console.WriteLine(st);
+            }
+
+
+        }
+    }
+}
