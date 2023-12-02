a9a3beb8f638fdb841ee2aabb6b4243eb886a444
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-12-02 11:37:36 +0200

    Dictionary, Extensions

---
+++ 14_ExtensionMethods/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace _14_ExtensionMethods
+{
+    static class ExampleExtensions
+    {
+        public static int NumberWords(this string data)
+        {
+            if (string.IsNullOrEmpty(data)) return 0;
+            return data.Split(new char[] { ' ', ',', '.' }, 
+                StringSplitOptions.RemoveEmptyEntries).Length;
+          
+        }
+        public static int NumberSymbols(this string data, char s)
+        {
+            if (string.IsNullOrEmpty(data)) return 0;
+            int count = 0;
+            foreach (char c in data)
+            {
+                if (c == s) count++;
+            }
+            return count;
+        }
+
+    }
+    class ExampleNameOf
+    {
+        public string Name { get; set; }
+        public ExampleNameOf(string FinishAction)
+        {
+            if(FinishAction == null)
+                throw new ArgumentNullException(nameof(FinishAction));
+
+            Name = FinishAction;
+        }
+       
+    }
+    internal class Program
+    {      
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a);
+            string str = "1";
+            string copy;//null
+            if(true)
+            {
+                int b = 10;
+                string str2 = "2";//0c2c2c
+                copy = str2;//0c2c2c = 0c2c2c
+                Console.WriteLine(b) ;
+                Console.WriteLine(str2) ;
+            }
+            //Console.WriteLine(b);
+           // Console.WriteLine(str2);
+            Console.WriteLine(copy);
+            Console.WriteLine(copy + str);
+            /*
+
+            try
+            {
+                ExampleNameOf nameOf = new ExampleNameOf(null);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Number of words : " + str.NumberWords()); 
+            Console.WriteLine("Number of symbol 'o' : " + str.NumberSymbols('o'));
+            int[] arr = new int[] { 2, 2, 2, 8,8, 8, 8, 3, 3, 3 };
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            IEnumerable<int> res = arr.Distinct();
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
+            */
+        }
+    }
+}
---
+++ 15_Dictionary/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+
+namespace _15_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        
+
+            Dictionary<string, string> countries = new Dictionary<string, String>();
+
+            countries.Add("UA", "Ukraine");
+            countries.Add("PL", "Poland");
+            countries.Add("USA", "United States");
+            countries.Add("FR", "France");
+            countries.Add("GB", "Great Britain");
+
+            foreach (KeyValuePair<string, string> item in countries)
+            {
+               
+                Console.WriteLine($"Key : {item.Key, -7} . Value : {item.Value, -15}");
+
+            }
+
+            var country = countries["USA"];//get
+            Console.WriteLine(country);
+            countries["USA"] = "America";//set
+            countries["IN"] = "India";//set
+            countries["RUN"] = "RUNNING";//set
+            countries.Remove("FR");
+            Console.WriteLine();
+            foreach (KeyValuePair<string, string> item in countries)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value,15}");
+            }
+            Console.WriteLine();
+            Dictionary<char, Person> people = new Dictionary<char, Person>();
+            people.Add('A', new Person() { Name = "Andriy" });
+            people.Add('P', new Person() { Name = "Petro" });
+            people.Add('M', new Person() { Name = "Mukola" });
+            people.Add('O', new Person() { Name = "Olga" });
+            //people.Add('A', new Person() { Name = "Anna" }); // exception
+
+
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value.Name,15}");
+            }
+
+            if (people.ContainsKey('A'))
+            {
+                people['A'] = new Person() { Name = "Anna" };
+                Console.WriteLine("Info was changed!!!!");
+            }
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value.Name,15}");
+            }
+
+            foreach (var item in people.Keys)
+            {
+                Console.WriteLine("Key : " + item);
+            }
+
+            foreach (var item in people.Values)
+            {
+                Console.WriteLine("Value : " + item.Name);
+            }
+        }
+        class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
